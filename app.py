dc12a857b780bf263e64df615b3442a8043de843
Shrashti Mittal <shrashtimittal323@gmail.com>
2025-12-13 22:35:33 +0530

    Implement user registration and authentication

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
 ﻿from database.db import initialize_database
+from auth.auth import register_user, login_user
+
+
+def main():
+    print("\nPersonal Finance Manager")
+    print("1. Register")
+    print("2. Login")
+    print("3. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        username = input("Username: ")
+        password = input("Password: ")
+        if register_user(username, password):
+            print("User registered successfully!")
+        else:
+            print("Username already exists.")
+
+    elif choice == "2":
+        username = input("Username: ")
+        password = input("Password: ")
+        if login_user(username, password):
+            print("Login successful!")
+        else:
+            print("Invalid credentials.")
+
+    elif choice == "3":
+        print("Goodbye!")
+        exit()
+
+    else:
+        print("Invalid option.")
+
 
 if __name__ == "__main__":
-
-    initialize_database()
-    print("Database initialized successfully!")
+    initialize_database()   # ensure DB exists
+    main()
---
+++ auth/auth.py
@@ -0,0 +1,41 @@
+import sqlite3
+import hashlib
+from database.db import get_connection
+
+
+def hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def register_user(username: str, password: str) -> bool:
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    hashed_pw = hash_password(password)
+
+    try:
+        cursor.execute(
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            (username, hashed_pw)
+        )
+        conn.commit()
+        return True
+    except sqlite3.IntegrityError:
+        return False
+    finally:
+        conn.close()
+
+def login_user(username: str, password: str) -> bool:
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    hashed_pw = hash_password(password)
+
+    cursor.execute(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        (username, hashed_pw)
+    )
+
+    user = cursor.fetchone()
+    conn.close()
+
+    return user is not None
